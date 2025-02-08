4d0ed18da06b845ccda632247628180819a17010
Naushad <naushad.ali@a5labs.co>
2025-02-08 19:11:29 +0530

    updated cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const cors = require('cors'); // Import the cors package
 const connectDB = require('./config/db');
 require('dotenv').config();
 const checkRole = require('./middleware/checkRole');
@@ -7,6 +8,8 @@ const authenticate = require('./middleware/authMiddleware');
 const app = express();
 app.set('view engine', 'ejs');  // Make sure to set the view engine for your app
 
+// Enable CORS for all routes
+app.use(cors());
 // Connect to MongoDB
 connectDB();
 // Middleware to parse URL-encoded data (e.g., form submissions)
