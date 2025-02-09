ff272d771eb29d1fe4c1b48550b41c38db4a744a
Naushad <naushad.ali@a5labs.co>
2025-02-09 20:16:52 +0530

    updated mail services

--- services/emailService.js
+++ services/emailService.js
@@ -2,16 +2,16 @@ const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config();
 
-// Create reusable transporter object using the default SMTP transport
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false, // True for 465, false for other ports
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+// // Create reusable transporter object using the default SMTP transport
+// const transporter = nodemailer.createTransport({
+//   host: process.env.EMAIL_HOST,
+//   port: process.env.EMAIL_PORT,
+//   secure: false, // True for 465, false for other ports
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
 
 console.log( process.env.EMAIL_USER, process.env.EMAIL_PASS );
 
